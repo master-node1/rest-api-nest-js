053c4a65b76b434fb57974011a41f299f1fb2dab
sateesh_s <sateesh.s@nuacem.com>
2024-07-31 21:46:41 +0530

    DB url from env

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,7 +9,7 @@ import { SearchModule } from './search/search.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/ott'),
+    MongooseModule.forRoot(process.env.DB_URL),
     MovieMOdule,
     UsersModule,
     AuthModule,
